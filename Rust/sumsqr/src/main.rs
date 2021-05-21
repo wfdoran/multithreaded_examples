5a1279976c9732ffc6cf51065f61c5f24e9bc0e4
William Doran <wfdoran4@yahoo.com>
2021-05-21 13:02:16 -0700

    rust sumsqr.

---
+++ Rust/sumsqr/src/main.rs
@@ -0,0 +1,35 @@
+use futures::executor::block_on;
+
+async fn hello(task_id: i64, num_tasks: i64, n: i64) -> i64 {
+    let mut rv: i64 = 0;
+    let mut i = task_id;
+
+    while i <= n {
+        rv += i * i;
+        i += num_tasks;
+    }
+
+    return rv;
+}
+
+fn main() {
+    let n: i64 = 1000;
+    let num_tasks: i64 = 5;
+
+    /*
+    let mut futures = vec![];
+
+    for i in 0..num_tasks {
+        futures.push(hello(i, num_tasks, n));
+    }
+    */
+
+    let futures: Vec<_> = (0..num_tasks).map(|i| hello(i,num_tasks,n)).collect();
+
+    let mut total: i64 = 0;
+    for f in futures {
+        total += block_on(f);
+    }
+
+    println!("{} {}", total, (n * (n - 1) * (2 * n - 1)) / 6);
+}
